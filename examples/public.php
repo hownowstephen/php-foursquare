8e9cc46c6aa3106c8b8c4b1e7bc3d719b05e201e
WhiteSpace-Strippers <email@whitespacestrippers.com>
2013-01-28 03:46:27 +0100

    WhiteSpace Strippers saved 0 bytes

--- examples/public.php
+++ examples/public.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	require_once("../src/FoursquareAPI.class.php");
 	$location = array_key_exists("location",$_GET) ? $_GET['location'] : "Montreal, QC";
 ?>
@@ -12,40 +12,40 @@
 <p>
 	Search for venues near...
 	<form action="" method="GET">
-		<input type="text" name="location" />
-		<input type="submit" value="Search!" />
+	<input type="text" name="location" />
+	<input type="submit" value="Search!" />
 	</form>
 <p>Searching for venues near <?php echo $location; ?></p>
 <hr />
-<?php 
+<?php
 	// Set your client key and secret
 	$client_key = "<your client key>";
 	$client_secret = "<your client secret>";
 	// Load the Foursquare API library
 	$foursquare = new FoursquareAPI($client_key,$client_secret);
-	
+
 	// Generate a latitude/longitude pair using Google Maps API
 	list($lat,$lng) = $foursquare->GeoLocate($location);
-	
+
 	// Prepare parameters
 	$params = array("ll"=>"$lat,$lng");
-	
+
 	// Perform a request to a public resource
 	$response = $foursquare->GetPublic("venues/search",$params);
 	$venues = json_decode($response);
 ?>
 	<ul>
-		<?php foreach($venues->response->venues as $venue): ?>
-			<li>
-				<?php 
-					echo $venue->name;
-					if(property_exists($venue->contact,"twitter")){
-						echo " -- follow this venue <a href=\"http://www.twitter.com/{$venue->contact->twitter}\">@{$venue->contact->twitter}</a>";
-					}
-				?>
-			
-			</li>
-		<?php endforeach; ?>
+	<?php foreach($venues->response->venues as $venue): ?>
+		<li>
+		<?php
+			echo $venue->name;
+			if(property_exists($venue->contact,"twitter")){
+			echo " -- follow this venue <a href=\"http://www.twitter.com/{$venue->contact->twitter}\">@{$venue->contact->twitter}</a>";
+			}
+		?>
+
+		</li>
+	<?php endforeach; ?>
 	</ul>
 </body>
 </html>
