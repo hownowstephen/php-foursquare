da23fa2ca0627e407c4e37ab3ca4df34bc7f467d
Zack Gilbert <zackgilbert@gmail.com>
2018-05-04 11:47:09 -0500

    Fix path to API class file.

--- examples/tokenrequest.php
+++ examples/tokenrequest.php
@@ -1,5 +1,5 @@
 <?php 
-	require_once("../src/FoursquareApi.class.php");
+	require_once("../src/FoursquareApi.php");
 	
 	// This file is intended to be used as your redirect_uri for the client on Foursquare
 	
